27d71ba274acce2fa2ef59e77112312383a06a0d
Almeida-91 <aplalmeida91@outlook.com>
2022-09-04 12:40:21 +0100

    Add size, getHead, getTail and at functions

--- linkedlist.js
+++ linkedlist.js
@@ -5,45 +5,76 @@ function newNode() {
     }
 }
 
-function newlinkedList(){
+function newLinkedList(){
     return{
         head : newNode(),
-        tail : newNode(), 
+        tail : newNode(),
+
+        prepend(value) {
+        if (this.head.value == null){
+            return append(value);
+        } else {
+            let toInsert = newNode();
+            toInsert.value = value;
+            toInsert.nextNode = this.head;
+            this.head = toInsert;
+        }
+        return this.head;
+        },
+
+        append(value) {
+            let toInsert = newNode();
+            toInsert.value = value;
+            if (this.head.value == null) {
+                this.head = toInsert;
+                this.tail = this.head;
+                return this.head;
+            } else {
+                this.tail = toInsert;
+                this.tail.nextNode = newNode();
+            }
+            return this.tail;
+        },
+
+        size() {
+            let cur = this.head;
+            let size = 0;
+            while ( cur.value != null){
+                size++;
+                cur = cur.nextNode;
+            }
+            return size;
+        },
+
+        getHead() {
+            return this.head;
+        },
+
+        getTail() {
+            return this.tail;
+        },
+
+        at(index) {
+            if (this.head.value == null) return 'List is empty!'
+            if (index < 0 || index == null) return 'Please enter a number equal or greater than 0';
+            let counter = 0;
+            let currentNode = this.head;
+            while (counter != index){
+                if (currentNode.nextNode == null) return 'Index is greater than the list size';
+                currentNode = currentNode.nextNode;
+                counter++;
+            }
+            return currentNode.value;         
+        },
+
+        
     }
 }
 
-let linkedList = newlinkedList();
 
 
-// add value to the end of the list
-// if head is null, head gets value and tail gets updated
-// else create new node and add it to the end of the list,
-// becoming the tail
-function append(value) {
-    let toInsert = newNode();
-    toInsert.value = value;
-    if (linkedList.head.value == null) {
-        linkedList.head = toInsert;
-        linkedList.tail = linkedList.head;
-        return linkedList.head;
-    } else {
-        linkedList.tail = toInsert;
-    }
-    return linkedList.tail;
-}
+let LinkedList = newLinkedList();
 
-// add value to the beginning of the list
-// if list is empty, append
-// else create a new node with pointer to head, change head to new node
-function prepend(value) {
 
-    if (linkedList.head.value == null){
-        return append(value);
-    } else {
-        let toInsert = newNode();
-        toInsert.value = value;
-        toInsert.nextNode = linkedList.head;
-        linkedList.head = toInsert;
-    }
-    return linkedList.head;
-}
+
+
